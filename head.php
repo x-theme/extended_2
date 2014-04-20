fd60ada623535f38087a94573eaf3ed658cddfae
JaeHo Song <thruthesky@gmail.com>
2014-04-21 00:10:55 +0800

    menu update: ...

--- head.php
+++ head.php
@@ -36,7 +36,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 					<li class='more-menu'><a href='javascript:void(0)'>More</a><li>
 					<li class='menu-3'><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_2'>질문답변</a></li>
 					<li class='menu-2'><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_6'>팁 정보공유</a></li>
-					<li class='menu-2'><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_3'>건의,요청</a></li>
+					<li class='menu-2'><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_1'>건의,요청</a></li>
 					<li class='menu-4'><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_5'>다운로드</a></li>
 					<li><a href='<?=g::url()?>/?module=update&action=list'>갤러리</a></li>
 				</ul>
@@ -59,7 +59,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 			<ul>
 				<li><a href='<?=G5_BBS_URL?>/write.php?bo_table=ms_www_2'>질문과답변</a></li>
 				<li><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_6'>팁 정보공유</a></li>
-				<li><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_3'>건의,요청</a></li>
+				<li><a href='<?=G5_BBS_URL?>/board.php?bo_table=ms_www_1'>건의,요청</a></li>
 				<li class='last-menu'><a href='<?=g::url()?>/?page=ms_www_5'>다운로드</a></li>
 			</ul>
 		<div class='close-menu'>X</div>
